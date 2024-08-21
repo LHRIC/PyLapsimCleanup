c4126b1eb6116e7387d46e70389f5afa42f6b468
tm1287 <tejas.maraliga@gmail.com>
2024-08-21 17:17:57 -0400

    Bringup rudimentary parameter loading with yaml_prop

--- main.py
+++ main.py
@@ -4,15 +4,21 @@ import yaml
 import os, sys;
 sys.path.insert(0, __file__.rsplit(os.sep,3)[0])
 
-from .yaml_prop.yaml_prop import PropertyLoader
+from yaml_prop import PropertyLoader
 
 # %%
 def main():
-    EXAMPLE_PATH = __file__.rsplit(os.sep,1)[0]
-    with open(os.path.join(EXAMPLE_PATH, 'conf.yml'), 'r') as file:
+    MODULE_PATH = __file__.rsplit(os.sep,1)[0]
+    with open(os.path.join(MODULE_PATH, 'vehicle_params.yml'), 'r') as file:
         data = yaml.load_all(file, Loader=PropertyLoader)
         for doc in data:
-            _pause = True
+            for subsys in doc:
+                for key, val in doc[subsys].items():
+                    try:
+                        print(key, val.unit, val.value)
+                    except:
+                        pass # not a constant param
+
 
 if __name__ == '__main__':
     main()
